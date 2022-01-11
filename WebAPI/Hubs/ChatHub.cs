8eeb9d65d1e4e6215f71288c544a222be82651e7
omerfurkandogruyol <omerfurkandogruyol@outlook.com>
2022-01-11 17:22:39 +0300

    first commit

---
+++ WebAPI/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebAPI.Hubs
+{
+
+    public class ChatHub : Hub<IChatClient>
+    {
+        public class ChatMessage
+        {
+            public string User { get; set; }
+
+            public string Message { get; set; }
+        }
+        public async Task SendMessage(ChatMessage message)
+        {
+            await Clients.All.ReceiveMessage(message);
+        }
+
+    }
+}
---
+++ WebAPI/Hubs/IChatClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using static WebAPI.Hubs.ChatHub;
+
+namespace WebAPI.Hubs
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(ChatMessage message);
+    }
+}
